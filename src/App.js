ebbab8ce51b0186c56f9ef16bcfb012d1cba619d
DineshReddy <ndkr2716@gmail.com>
2022-12-02 20:15:13 +0530

    Assignment Completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function image() {
+  return (
+    <div className="App">
+      <img src='https://geekster-in.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F3f668fa5-ba79-40cd-afc1-5a3c649f27ae%2FUntitled.png?table=block&id=0e74f2f2-2963-41d0-94f7-b66795e4e0bb&spaceId=d21b1a65-81c4-4c29-a7ac-4ece53e36277&width=1440&userId=&cache=v2' alt='air-bnb' className='image'></img>
+    </div>
+  );
+}
+
+export default image;
